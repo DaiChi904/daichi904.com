63e2014c89cbdf4a07474f054cde1efaffc7671f
Kokko <minimi.904.horo@gmail.com>
2024-03-10 20:05:12 +0900

    minor style change

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,20 +5,20 @@ function Header() {
         <>
           <div className="flex text-zinc-100 w-screen items-center justify-center border-b-2 border-dashed pb-2 mt-2">
             <ul className="flex justify-center">
-              <li className="text-2xl px-10 border-2 border-zinc-100 mx-0.5 rounded">
-                <Link href="/">Home</Link>
+              <li className="text-2xl border-2 border-zinc-100 mx-0.5 rounded text-center">
+                <Link className="block w-full h-full px-10 hover:bg-purple-600 hover:opacity-95" href="/">Home</Link>
               </li>
-              <li className="text-2xl px-10 border-2 border-zinc-100 mx-0.5 rounded">
-                <Link href="/profile">Profile</Link>
+              <li className="text-2xl border-2 border-zinc-100 mx-0.5 rounded">
+                <Link className="block w-full h-full px-10 hover:bg-purple-600 hover:opacity-95" href="/profile">Profile</Link>
               </li>
-              <li className="text-2xl px-10 border-2 border-zinc-100 mx-0.5 rounded">
-                <Link href="/portfolio">Portfolio</Link>
+              <li className="text-2xl border-2 border-zinc-100 mx-0.5 rounded">
+                <Link className="block w-full h-full px-10 hover:bg-purple-600 hover:opacity-95" href="/portfolio">Portfolio</Link>
               </li>
-              <li className="text-2xl px-10 border-2 border-zinc-100 mx-0.5 rounded">
-                <Link href="/Blog">Blog</Link>
+              <li className="text-2xl border-2 border-zinc-100 mx-0.5 rounded">
+                <Link className="block w-full h-full px-10 hover:bg-purple-600 hover:opacity-95" href="/Blog">Blog</Link>
               </li>
-              <li className="text-2xl px-10 border-2 border-zinc-100 mx-0.5 rounded">
-                <Link href="/SNS">SNS</Link>
+              <li className="text-2xl border-2 border-zinc-100 mx-0.5 rounded">
+                <Link className="block w-full h-full px-10 hover:bg-purple-600 hover:opacity-95" href="/SNS">SNS</Link>
               </li>
             </ul>
           </div>
