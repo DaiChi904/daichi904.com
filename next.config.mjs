edd1aeac15971f9179bfcd0c72197b134e22a365
Kokko <minimi.904.horo@gmail.com>
2024-03-14 18:22:30 +0900

    minor fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const prod = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-    basePath: prod ? "/my-home-page" : "",
     output: "export",
     images: { unoptimized: true } ,
 };
