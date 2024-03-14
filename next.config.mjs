2161609164d347c9ca5be0259bf712900ac8993b
Kokko <minimi.904.horo@gmail.com>
2024-03-14 17:11:00 +0900

    minor change

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    export: "output",
-    basePath: process.env.GITHUB_ACTIONS && "/my-home-page",
-    trailingSlash: true,
+    basePath: "/my-home-page",
+    output: "export",
+    images: { unoptimized: true } ,
 };
 
 export default nextConfig;
