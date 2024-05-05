c0223aea66fc63bb23a1edbe61c3782cce41e27b
Daichi Uematsu <minimi.904.horo@gmail.com>
2024-05-06 00:26:24 +0900

    added new gyagus and fixed layout

--- src/app/profile/gyagu/page.tsx
+++ src/app/profile/gyagu/page.tsx
@@ -4,32 +4,44 @@ function Portfolio() {
     return(
         <>
             <BaseLayout>
-                <main className="flex flex-col grow">
-                    <div className="flex justify-center">
-                        <h1 className="text-4xl my-2">ギャグ一覧</h1>
+                <main className="flex justify-center">
+                    <div className="flex w-11/12 justify-center">
+                        <div className="flex flex-col">
+                            <h1 className="text-4xl my-3">ギャグ一覧</h1>
+                            <ol className="text-2xl my-2">
+                                <li>水こぼした。おわーたー。</li>
+                                <li>コメントには気持ちをコメント。</li>
+                                <li>子供めっちゃいるど(child)。</li>
+                                <li>物を落とした音とした。</li>
+                                <li>アルミ缶の上にあるミカン。</li>
+                                <li>日本の海にじゃっぱーん。</li>
+                                <li>大義を果たすのがたいぎい。</li>
+                                <li>この会場はでかいじょう。</li>
+                                <li>泣くくらい(cry)悲しい。</li>
+                                <li>クレヨンくれよん。</li>
+                                <li>調味料で超魅了。</li>
+                                <li>慣行の観光を敢行する。</li>
+                                <li>この映像とてもえいぞう。</li>
+                                <li>思い出は重いで...</li>
+                                <li>台湾にいきたいわん！</li>
+                                <li>エイリアンの歯めっちゃ鋭利やん。</li>
+                                <li>石が落ちた。ストーン！</li>
+                                <li>内科医にならないかい？</li>
+                                <li>キッチンをきっちんと掃除する。</li>
+                                <li>豚をぶったブッタ。</li>
+                                <li>口をあんぐり開けて怒る(angry)。</li>
+                                <li>太陽にあたりたいよう。</li>
+                                <li>骨が折れた。ほーねー。</li>
+                                <li>携帯を携帯する。</li>
+                                <li>高度なコード。</li>
+                                <li>小樽におったる。</li>
+                                <li>稚内がどこだかわっかない。</li>
+                                <li>冬季に陶器を投棄する。</li>
+                                <li>関東にいかんとう。</li>
+                                <li>いけん意見。</li>
+                            </ol>
+                        </div>
                     </div>
-                    <ol className="text-2xl my-2">
-                        <li>水こぼした。おわーたー。</li>
-                        <li>コメントには気持ちをコメント。</li>
-                        <li>子供めっちゃいるど。</li>
-                        <li>物を落とした音とした。</li>
-                        <li>その友達のことにはフレンドこう。</li>
-                        <li>アルミ缶の上にあるミカン。</li>
-                        <li>日本の海にじゃっぱーん。</li>
-                        <li>大義を果たすのがたいぎい。</li>
-                        <li>この会場はでかいじょう。</li>
-                        <li>泣くくらい(cry)悲しい。</li>
-                        <li>共産主義への協賛主義。</li>
-                        <li>クレヨンくれよん。</li>
-                        <li>調味料で超魅了。</li>
-                        <li>慣行の観光を敢行する。</li>
-                        <li>この映像とてもえいぞう。</li>
-                        <li>思い出は重いで...</li>
-                        <li>台湾にいきたいわん！</li>
-                        <li>エイリアンの歯めっちゃ鋭利やん。</li>
-                        <li>石が落ちた。ストーン！</li>
-                        <li>内科医にならにかい？</li>
-                    </ol>
                 </main>
             </BaseLayout>
         </>
