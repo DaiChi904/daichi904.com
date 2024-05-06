0196bc93b3a3115c3616b32b0740582114c8d677
Daichi Uematsu <minimi.904.horo@gmail.com>
2024-05-06 22:12:58 +0900

    updated the page of contact

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,11 +1,44 @@
 import BaseLayout from "@/layouts/BaseLayout";
+import PORItemContainer from "@/portfolioItem/PORItemContainer";
+import PORItemLogo from "@/portfolioItem/PORItemLogo";
+import PORItemTitle from "@/portfolioItem/PORItemTitle";
+import PORLowerItem from "@/portfolioItem/PORLowerItem";
+import PORUpperItem from "@/portfolioItem/PORUpperItem";
+import Link from "next/link";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
 
 function Contact() {
     return(
         <>
             <BaseLayout>
-                <main className="flex grow justify-center items-center">
-                    <h1 className="text-4xl">無いです。</h1>
+                <main className="flex flex-col grow justify-center items-center">
+                    <PORItemContainer>
+                        <PORUpperItem>
+                            <PORItemLogo>
+                                <BiLogoGmail size={50}/>
+                            </PORItemLogo>
+                            <PORItemTitle>
+                                Gmail
+                            </PORItemTitle>
+                        </PORUpperItem>
+                        <PORLowerItem> 
+                            daichi904.22219@gmail.com                  
+                        </PORLowerItem>
+                    </PORItemContainer>
+                    
+                    <PORItemContainer>
+                        <Link href="https://github.com/DaiChi904">
+                            <PORUpperItem>
+                                <PORItemLogo>
+                                    <FaGithub size={50}/>
+                                </PORItemLogo>
+                                <PORItemTitle>
+                                    Github
+                                </PORItemTitle>
+                            </PORUpperItem>
+                        </Link>
+                    </PORItemContainer>
                 </main>
             </BaseLayout>
         </>
