f0e495be28a943768e547850a9598ff1b57dbeda
daichi904 <minimi.904.horo@gmail.com>
2024-06-24 13:04:16 +0900

    minor change

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -11,24 +11,22 @@ export default function BaseLayout({ children }: Readonly<{ children: React.Reac
         setIsReloaded(true);
     }, 500);
     return (
-        <>
-            <div className="flex max-h-full min-h-screen w-screen flex-col justify-between">
-                <div className="mt-2 flex w-full items-center justify-center border-b-2 border-dashed pb-2">
-                    <Header />
-                </div>
-                <div
-                    className={
-                        isReloaded
-                            ? "flex grow flex-col opacity-100 transition-opacity duration-700 ease-in-out"
-                            : "opacity-0 transition-opacity duration-700 ease-in-out"
-                    }
-                >
-                    {children}
-                </div>
-                <div className="mb-3 flex justify-center">
-                    <Footer />
-                </div>
+        <div className="flex max-h-full min-h-screen w-screen flex-col justify-between">
+            <div className="mt-2 flex w-full items-center justify-center border-b-2 border-dashed pb-2">
+                <Header />
             </div>
-        </>
+            <div
+                className={
+                    isReloaded
+                        ? "flex grow flex-col opacity-100 transition-opacity duration-700 ease-in-out"
+                        : "opacity-0 transition-opacity duration-700 ease-in-out"
+                }
+            >
+                {children}
+            </div>
+            <div className="mb-3 flex justify-center">
+                <Footer />
+            </div>
+        </div>
     );
 }
