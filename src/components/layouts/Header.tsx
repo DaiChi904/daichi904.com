30293790ce30220c56a23a8bd65fd007342b8956
daichi904 <minimi.904.horo@gmail.com>
2024-06-24 13:04:27 +0900

    minor change

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,35 +1,57 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function Header() {
+    const path = usePathname();
     return (
         <div className="flex w-full flex-row flex-wrap items-center justify-center">
             <Link
                 href="/"
-                className="flex h-full basis-1/6 items-center justify-center rounded border-2 hover:rounded hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                className={
+                    path === "/"
+                        ? "flex h-full basis-1/6 items-center justify-center rounded border-2 bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                        : "flex h-full basis-1/6 items-center justify-center rounded border-2 hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                }
             >
                 <p className="md:text-base sm:text-sm l-md:text-2xl">Home</p>
             </Link>
             <Link
                 href="/profile"
-                className="flex h-full basis-1/6 items-center justify-center rounded border-2 hover:rounded hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                className={
+                    path === "/profile"
+                        ? "flex h-full basis-1/6 items-center justify-center rounded border-2 bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                        : "flex h-full basis-1/6 items-center justify-center rounded border-2 hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                }
             >
                 <p className="md:text-base sm:text-sm l-md:text-2xl">Profile</p>
             </Link>
             <Link
                 href="/portfolio"
-                className="flex h-full basis-1/6 items-center justify-center rounded border-2 hover:rounded hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                className={
+                    path === "/portfolio"
+                        ? "flex h-full basis-1/6 items-center justify-center rounded border-2 bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                        : "flex h-full basis-1/6 items-center justify-center rounded border-2 hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                }
             >
                 <p className="md:text-base sm:text-sm l-md:text-2xl">Portfolio</p>
             </Link>
             <Link
                 href="/blog"
-                className="flex h-full basis-1/6 items-center justify-center rounded border-2 hover:rounded hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                className={
+                    path === "/blog"
+                        ? "flex h-full basis-1/6 items-center justify-center rounded border-2 bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                        : "flex h-full basis-1/6 items-center justify-center rounded border-2 hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                }
             >
                 <p className="md:text-base sm:text-sm l-md:text-2xl">Blog</p>
             </Link>
             <Link
                 href="/contact"
-                className="flex h-full basis-1/6 items-center justify-center rounded border-2 hover:rounded hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                className={
+                    path === "/contact"
+                        ? "flex h-full basis-1/6 items-center justify-center rounded border-2 bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                        : "flex h-full basis-1/6 items-center justify-center rounded border-2 hover:bg-purple-600 hover:opacity-95 md:mx-0.5 md:px-0.5 l-md:mx-1 l-md:px-1"
+                }
             >
                 <p className="md:text-base sm:text-sm l-md:text-2xl">Contact</p>
             </Link>
