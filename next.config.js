8fac94017f77d5d914c3ed05656e011bad8ba7cd
daichi904 <minimi.904.horo@gmail.com>
2024-09-02 17:55:30 +0900

    fixed sub directory path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 /* 公開時のサブディレクトリ */
-const SUB_DIRECTORY = "/my-home-page";
+const SUB_DIRECTORY = "/daichi904.com";
 
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV === "production";
