a2b26ca1c82185c7215e0786840285799bfd2d43
daichi904 <minimi.904.horo@gmail.com>
2024-11-01 12:47:32 +0900

    minor fix

--- src/components/element/ConfiguredImage.tsx
+++ src/components/element/ConfiguredImage.tsx
@@ -17,8 +17,10 @@ interface Props {
  * Define fit to true, if you want to fit the size of image to parent's content.
  */
 export default function ConfiguredImage({ src, useOutsideSrc, maxWidth, maxHeight, alt, fit, className }: Props) {
+    const _maxWidth = fit ? 10000 : maxWidth;
+    const _maxHeight = fit ? 10000 : maxHeight;
     const _src = !useOutsideSrc ? BASE_PATH + src : src;
     return (
-        <Image src={_src} width={maxWidth} height={maxHeight} alt={alt} className={`flex ${fit && "size-fit"} ${className}`} />
+        <Image src={_src} width={_maxWidth} height={_maxHeight} alt={alt} className={`flex ${fit && "size-fit"} ${className}`} />
     );
 }
